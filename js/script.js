7935e159847d64aa5e3fdf18d41849aa3db2842f
Victoria Marques <32624288+vicckm@users.noreply.github.com>
2020-06-15 13:09:35 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function rodaJogo() {
+
+
+
+function primeiraFase() {
+		
+ 	var button = document.querySelector ("button"); 
+	
+
+	var pergunta = document.querySelector('.texto');
+			p = "oi"	
+}
+
+
+
+
+}
+rodaJogo();
